dac8f39f18964ab710e3abed7bcda177b16b5853
Alex A <alex.av936@gmail.com>
2022-08-14 20:53:49 -0500

    Have to return a string with flask

--- application.py
+++ application.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask
 
 from queryFunctions import insert_activity_db, get_single_user_total_points_db
@@ -16,7 +18,8 @@ def insert_activity():
 
 @application.route('/getSingleUserTotalPoints')
 def get_single_user_total_points():
-    return get_single_user_total_points_db("1234-1234-1234-1234")
+    totalPoints = get_single_user_total_points_db("1234-1234-1234-1234")
+    return str(totalPoints)
 
 if __name__ == '__main__':
     application.debug = True
